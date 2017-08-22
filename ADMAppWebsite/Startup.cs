7ef3bb29a64e974e89360695bae71f1224f6c661
devopsculture <devopsculture@gmail.com>
2017-08-22 17:43:57 +0530

    Adding test project

---
+++ ADMAppWebsite/Startup.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens.Jwt;
+
+namespace ADMAppWebsite
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions {
+                AuthenticationScheme = "Cookies"
+            });
+
+            JwtSecurityTokenHandler.DefaultOutboundClaimTypeMap.Clear();
+            
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectOptions {
+                AuthenticationScheme = "oidc",
+                SignInScheme = "Cookies",
+                Authority = "http://wsauthservicedevopsdeploy1.azurewebsites.net/",
+                RequireHttpsMetadata = false,
+                ClientId = "admapp",
+                SaveTokens = true
+            //}).UseOpenIdConnectAuthentication(new OpenIdConnectOptions {
+            //    AuthenticationScheme = "oidc",
+            //    DisplayName = "Azure AD",
+            //    SignInScheme = "Cookies",
+            //    Authority = $"https://login.microsoftonline.com/{tenantId}",
+            //    RequireHttpsMetadata = false,
+            //    ClientId = "2beaf9f4-9eeb-4219-983e-ae8e454b70e6",
+            //    SaveTokens = true
+            });
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
